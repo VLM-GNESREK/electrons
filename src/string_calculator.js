34852b04bd92be2440d3145e0442b85f3b305744
Vincent Leonardo Müller PC2 <vincentleonardo.mueller@gmail.com>
2025-06-11 22:43:28 +0200

    TC0 IN PROGRESS (Testing) - PC2 Driver

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,22 +1,7 @@
 // StringCalculator.js
 
 function StringCalculator() {}
-StringCalculator.prototype.add = function(string_numbers) 
+StringCalculator.prototype.add = function(array) 
 {
-	if(string_numbers === '') return 0;
 
-	let sum = 0;
-
-	const numberArray = string_numbers.split(',');
-
-	for(let i = 0; i < numberArray.length; i++)
-	{
-		const number = parseInt(numberArray[i]);
-		if(!isNaN(number))
-		{
-			sum += number;
-		}
-	}
-
-	return sum;
 };
